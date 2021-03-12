9879e32ec4ec9f5597f4f48a49dd834e8ffcad83
glanzwulf <ethel@live.com.pt>
2021-03-12 22:14:34 +0000

    changes to rpofile page, add button and some css

--- mentalh/src/Components/Profile.jsx
+++ mentalh/src/Components/Profile.jsx
@@ -4,18 +4,20 @@ import './Profile.css'
 
 const Profile = () => {
     let username = 'Vlad';
-    const Date = moment().format('MMMM Do');
     const Day = moment().format('dddd'); 
-    
+
     return (
         <div className="Profile">
             <div className="ProfileHeader">
                 <h1>Hello {username}</h1>
                 <h3>Today is: {Day}</h3>
+                <p>We would like to get to know you a little bit better. What's your name?</p>
+                <button className="profile-name-btn">This will be a button with onclick open form</button>
             </div>
             <div className="MoodsOverTime">
                 <h1>Moods over time</h1>
                 <img className="graph" src="http://expatchild.com/wp-content/uploads/2012/11/Culture-shock-graph-basic.jpg" />
+                <p>This is a placeholder</p>
             </div>
         </div>
 )
