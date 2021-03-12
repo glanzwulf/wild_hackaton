b4ec72bcdaa5bdbb087ac7778b0459a7c97a6873
glanzwulf <ethel@live.com.pt>
2021-03-12 19:07:17 +0000

    create app skeleton

---
+++ mentalh/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import Mood from './Components/Mood'
+import MoodText from './Components/MoodText'
+import ActivityList from './Components/ActivityList'
+import ActivityDetails from './Components/ActivityDetails'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Header />
+        <Mood />
+        <MoodText />
+          <Switch>
+            <Route exact path='/' component={ActivityList} />
+            <Route path='/:name' component={ActivityDetails} />
+          </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
